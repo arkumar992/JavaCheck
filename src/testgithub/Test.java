08058f007a6cc53e95c691da9086c85856409a02
suresh babu <suresh.babu@pixintprojects.com>
2020-04-14 11:03:36 +0530

    firt user commit

---
+++ src/testgithub/Test.java
@@ -0,0 +1,15 @@
+package testgithub;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=9;
+		int b =8;
+		int c = a+b;
+		System.out.println("test data: " + c);
+		
+	}
+
+}
